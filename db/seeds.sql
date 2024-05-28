b42caa8a748b132f3361ec5c4a3eee836c8665d8
yong woo Yun <yongwooyun@hotmail.com>
2024-05-28 13:55:41 -0700

    adding seed data based on exmaple

--- db/seeds.sql
+++ db/seeds.sql
@@ -7,14 +7,20 @@ INSERT INTO role (title, salary, department_id)
 VALUES 
     ('Sales Lead', 100000, 1),
     ('Salesperson', 80000, 1),
+    ('Lead Engineer', 150000, 2),
     ('Software Engineer', 120000, 2),
-    ('Accountant', 90000, 3),
-    ('Legal Team Lead', 110000, 4);
+    ('Account Manager', 160000, 3),
+    ('Accountant', 125000, 3),
+    ('Legal Team Lead', 250000, 4),
+    ('Lawyer', 190000, 4);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES 
     ('John', 'Doe', 1, NULL),
-    ('Jane', 'Smith', 2, 1),
-    ('Mark', 'Brown', 3, NULL),
-    ('Mike', 'Johnson', 4, 3),
-    ('Samantha', 'Green', 5, NULL);
+    ('Mike', 'Chan', 2, 1),
+    ('Ashley', 'Rodriguez', 3, NULL),
+    ('Kevin', 'Tupik', 4, 3),
+    ('Kunal', 'Singh', 5, NULL),
+    ('Malia', 'Brown', 6, 5),
+    ('Sarah', 'Lourd', 7, Null),
+    ('Tom', 'Allen', 8, 7);
